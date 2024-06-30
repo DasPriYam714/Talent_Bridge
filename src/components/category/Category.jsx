8773faa949e281efba190edd54541c9062089687
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-06-30 21:09:36 +0600

    add some updates

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -4,15 +4,15 @@ const Category = ({category}) => {
     const {category_name, logos, availability} = category;
     return (
         <div>
-            <div>
-                <div>
+            <div className="border border-cyan-300 p-6">
+                <div className="size-24">
                 <img src={logos} alt="logo" />
                 
                 </div>
-                <div>
+                <div className="text-5 font-extrabold text-[#474747]">
                 <h2>{category_name}</h2>
                 </div>
-                <div>
+                <div className="text-4 font-medium text-[#A3A3A3]">
                     <p>{availability}</p>
                 </div>
             </div>
--- src/components/category/JobCategory.jsx
+++ src/components/category/JobCategory.jsx
@@ -15,7 +15,7 @@ const JobCategory = () => {
                     <p className="text-4xl font-extrabold mt-10">Job Category List</p>
                     <p className="font-medium mt-5 mb-10">Explore thousands of job opportunities with all the information you need. Its your future</p>
                 </section>
-                <div className="flex flex-col sm:flex-row gap-10">
+                <div className="flex flex-col sm:flex-row justify-between text-center">
                     {
                         categories.map(category=> <Category key={category.id} category={category}></Category>)
                     }
