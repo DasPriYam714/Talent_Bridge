e6ff8cce55bc23a06e1945f78ba156f12fd65ad2
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-07-09 12:13:35 +0600

    add logo

--- src/components/appliedJobs/Job.jsx
+++ src/components/appliedJobs/Job.jsx
@@ -1,19 +1,29 @@
-
+import { GoLocation } from "react-icons/go";
+import { HiCurrencyBangladeshi } from "react-icons/hi2";
 
 const Job = ({job}) => {
-    const {logo,job_title,job_description} =job;
+    const {logo,company_name,job_title,job_description,remote_or_onsite,job_type,location,salary} =job;
     return (
         <div className="card card-compact bg-base-100  shadow-xl">
         <figure>
           <img
             src={logo}
-            alt="Shoes" />
+            alt="company_logo"
+            className="p-10 max-auto" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{job_title}</h2>
-          <p>{job_description}</p>
+          <h2 className="card-title text-2xl font-bold">{company_name}</h2>
+          <p className="text-xl ">{job_title}</p>
+          <div>
+            <button className="px-5 py-2 border border-[#7E90FE] font-extrabold rounded mr-5 text-[#9873FF]">{remote_or_onsite}</button>
+            <button className="px-5 py-2 border border-[#7E90FE] font-extrabold rounded mr-5 text-[#9873FF]">{job_type}</button>
+          </div>
+          <div className="flex flex-col sm:flex-row text-xl">
+            <h2 className="flex my-2 mr-12"><GoLocation className="text-2xl mr-2"></GoLocation>{location}</h2>
+            <h2 className="flex my-2 mr-2"><HiCurrencyBangladeshi className="text-3xl mr-2"></HiCurrencyBangladeshi>{salary}</h2>
+          </div>
           <div className="card-actions ">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">View details</button>
           </div>
         </div>
       </div>
