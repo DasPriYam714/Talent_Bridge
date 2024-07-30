26abd99590dd189b23c0f542f14dcddc1d826c7e
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-07-30 11:30:33 +0600

    add job description part 1

--- src/components/job details/JobDetail.jsx
+++ src/components/job details/JobDetail.jsx
@@ -1,5 +1,6 @@
 
 import { useLoaderData, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const JobDetail = () => {
@@ -12,8 +13,31 @@ const JobDetail = () => {
     console.log(job);
     return (
         <div>
-            this is my job.
             <h2>Job details of:{job.job_title}</h2>
+            <div className="grid gap-5 md:grid-cols-4 grid-cols-1">
+                <div className="  md:col-span-3">
+                    <h2 className="font-bold text-lg pb-5">Job Description: <span className="font-medium text-[#757575]">{job.job_description}</span></h2>
+                    <h2 className="font-bold text-lg pb-5">Job Responsibility: <span className="font-medium text-[#757575]">{job.job_responsibility}</span></h2>
+                    <div className="pb-5">
+                    <h2 className="font-bold text-lg pb-3 ">Educational Requirement: </h2>
+                        <span className="font-medium text-[#757575] pb-5">{job.educational_requirements}</span>
+
+
+                    </div>
+                    <div className="pb-5">
+                    <h2 className="font-bold text-lg pb-3 ">Experiences: </h2>
+                        <span className="font-medium text-[#757575] pb-5">{job.experiences}</span>
+
+
+                    </div>
+                   
+
+                </div>
+                <div className="border flex flex-col"> sidebar
+
+                    <Link><button className="btn btn-primary text-lg w-full bg-gradient-to-r from-indigo-500">Apply</button></Link>
+                </div>
+            </div>
 
         </div>
     );
